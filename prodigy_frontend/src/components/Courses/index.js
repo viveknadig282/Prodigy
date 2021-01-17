6c9ef080875e0769a5315a4e90f03f0ec91353e3
Brayden Tam <brayden.tam@gmail.com>
2021-01-16 22:44:39 -0800

    trying to get button id

--- prodigy_frontend/src/components/Courses/index.js
+++ prodigy_frontend/src/components/Courses/index.js
@@ -5,20 +5,21 @@ import StarRatings from 'react-star-ratings';
 import {CourseLink} from './courseElement'
 
 
-const Course = () => {
-    function SubmitSignupHandler(e){
-        e.preventDefault()
 
-    }
+const Course = () => {
+        function reply_click(e, clicked_id){
+            e.preventDefault()
+            console.log(clicked_id);
+        }
         return (
             <>
             <div class="Overall_Container">
 
                 {courses.courses.map((item, i) => (
-                    <a href="#" onClick={SubmitSignupHandler}>
+                    // eslint-disable-next-line
+                    <a href="#">
                     {/* <SubmitSignupLink to="/" class="submitButton">Submit</SubmitSignupLink> */}
-                
-                    <button class="Course_container" key={i}><span><CourseLink to="/infopage">
+                    <button class="Course_container" id={i} onClick={() => this.reply_click(item.id)}><span><CourseLink to="/infopage">
                         <h3 class="Course_name">{item.name}</h3>
                         <h3 class="Course_teacher">{item.teacher}</h3>
                         <h3 class="Course_cost">{item.cost}</h3>
