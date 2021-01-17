a7484287017699fc903733f33fe9e618dd4dc955
Aditya Pawar <aditya.pawar@warriorlife.net>
2021-01-16 19:16:29 -0800

    added validation for user input in react

--- prodigy_frontend/src/components/Login/index.js
+++ prodigy_frontend/src/components/Login/index.js
@@ -4,56 +4,103 @@ import './login.css'
 
 import {SubmitLink} from './LoginElement'
 
-const Login = () => {
-    function SubmitHandler(e){
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            validity: "",
+            passwordStatus: "",
+            usernameStatus: ""
+        }
+    }
+    SubmitHandler = async (e) => {
         e.preventDefault()
         console.log(document.getElementById("username").value);
         console.log(document.getElementById("password").value);
-        const data = {
-            username: document.getElementById("username").value,
-            password: document.getElementById("password").value 
-        }
-        const config = {
+        this.setState({
+            passwordStatus: ""
+        })
+        let data = new FormData();
+        data.append('username', document.getElementById("username").value);
+        data.append('password', document.getElementById("password").value);
+        let response = await axios({
+            method: 'post',
             url: "http://127.0.0.1:8000/accounts/login",
-            method: "POST",
-            headers: {"Content-Type": "multipart/form-data"},
             data: data,
-        }
-        console.log(config)
-        axios(config)
-            .then(response => {
-                console.log("request done")
-                console.log(response.data)
+            headers: {'Content-Type': 'multipart/form-data' }
             })
-            .catch(error => {
-                console.log(error)
-            });
-
+        
+        if (response.data.hasOwnProperty('valid')) {
+            if (!response.data.valid) {
+                this.setState({
+                    passwordStatus: "",
+                    usernameStatus: "",
+                    validity: "The username or password was incorrect"
+                })
+            } else {
+                this.setState({
+                    passwordStatus: "",
+                    usernameStatus: "",
+                    validity: "Redirecting..."
+                }) 
+            }
+        }
+        
+        else if (response.data.hasOwnProperty('error')) {
+            if (response.data.error.hasOwnProperty('password')) {
+                this.setState({
+                    passwordStatus: response.data.error.password[0]
+                })
+            } else {
+                this.setState({
+                    validity: "",
+                    passwordStatus: ""
+                }) 
+            }
+            if (response.data.error.hasOwnProperty('username')) {
+                this.setState({
+                    validity: "",
+                    usernameStatus: response.data.error.username[0]
+                })
+            } else {
+                this.setState({
+                    usernameStatus: ""
+                }) 
+            }
+        }
     }
-    return (
-        <>
+    render() {
+    
+    return <>
         <section>
         <form>
+            
             <div class="spaceTop">
+            <label className="error">{this.state.validity}</label>
                 <label htmlFor="username" class="labelLogin">Username:</label>
-                <input type="text" name="username" id="username" class="loginInput"/>
+                <input type="text" name="username" id="username" className="loginInput"/>
+                <br></br>
+                <label className="error">{this.state.usernameStatus}</label>
             </div>
             <div>
                 <label htmlFor="password" class="labelLogin">Password:</label>
                 <input type="password" name="password" id="password" class="loginInput"/>
+                <br></br>
+                <label className="error">{this.state.passwordStatus}</label>
             </div>
             <div class="container1">
             <input type="submit" class="submit"/>
             {/* <Submit /> */}
             {/* eslint-disable-next-line */}
-            <a href="#" onClick={SubmitHandler}>
-                <SubmitLink to="/" class="submitButton">Submit</SubmitLink>
-            </a>
+            <button className="submitButton" href="#" onClick={this.SubmitHandler}>
+                Submit
+            </button>
         </div>
         </form>
         </section>
         </>
-    )
+    
+    }
 }
 
 export default Login;
