f574e832b277cdbb6c84aaa00139bfc11cd5a003
Brayden Tam <brayden.tam@gmail.com>
2021-01-17 12:21:40 -0800

    added call

--- Video Chat Application/VideoButton/videobutton.js
+++
@@ -1,8 +0,0 @@
-const videoButton = document.getElementById("video_button")
-
-function openVideo(event) {
-    event.preventDefault();
-    window.open(`https://36b4692e8330.ngrok.io`, "_blank");
-}
-
-videoButton.addEventListener('click', openVideo);
--- prodigy_frontend/src/pages/infopage.js
+++ prodigy_frontend/src/pages/infopage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Infopage from '../components/Infopage';
 
 const infopage = () => {
     return (
         <>
-            
+            <Infopage></Infopage>
         </>
     )
 }
