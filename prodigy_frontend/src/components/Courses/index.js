ea763c800ae9c2991d6894beb8aea767bce55618
Brayden Tam <brayden.tam@gmail.com>
2021-01-16 22:08:06 -0800

    fix

--- prodigy_frontend/src/components/Courses/index.js
+++ prodigy_frontend/src/components/Courses/index.js
@@ -2,17 +2,23 @@ import React from 'react'
 import courses from './courseObject.json'
 import './course.css'
 import StarRatings from 'react-star-ratings';
-
+import {CourseLink} from './courseElement'
 
 
 const Course = () => {
+    function SubmitSignupHandler(e){
+        e.preventDefault()
 
+    }
         return (
             <>
             <div class="Overall_Container">
 
                 {courses.courses.map((item, i) => (
-                    <button class="Course_container" key={i}><span>
+                    <a href="#" onClick={SubmitSignupHandler}>
+                    {/* <SubmitSignupLink to="/" class="submitButton">Submit</SubmitSignupLink> */}
+                
+                    <button class="Course_container" key={i}><span><CourseLink to="/infopage">
                         <h3 class="Course_name">{item.name}</h3>
                         <h3 class="Course_teacher">{item.teacher}</h3>
                         <h3 class="Course_cost">{item.cost}</h3>
@@ -23,8 +29,10 @@ const Course = () => {
                         starRatedColor="RGB(255,255,0)"
                         starEmptyColor="RGB(255,255,255)"
                     />
+                    </CourseLink>
                     </span>
                     </button>
+                    </a>
                 ))}
 
             </div>
