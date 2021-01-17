bd6eda1bcfe8103d1fc3751f608dfbd945dd8972
Aditya Pawar <aditya.pawar@warriorlife.net>
2021-01-17 11:41:37 -0800

    made class serializer more modular

--- prodigy_site/classes/serializers.py
+++ prodigy_site/classes/serializers.py
@@ -2,9 +2,30 @@ from rest_framework import serializers
 from .models import Class, Review
 
 
-class ClassSerializer(serializers.ModelSerializer):
-    average_rating = Review
-
+class ClassModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class
         fields = ('id', 'name', 'subject', 'teacher', 'cost', 'desc')
+
+
+class ClassSerializer():
+    def __init__(self, class_object):
+        self.serialized = ClassModelSerializer(class_object).data
+        reviews = Review.objects.filter(class_for=class_object)
+        teacher = class_object.teacher
+        subject = class_object.subject
+
+        self.serialized["subject"] = subject.name
+        self.serialized["teacher"] = teacher.user.username
+
+        if len(reviews) == 0:
+            self.serialized['avr_reviews'] = 0
+        else:
+            sum = 0
+            for review in reviews:
+                sum += review.rating
+
+            self.serialized['avr_reviews'] = sum/len(reviews)
+
+    def get_serialized(self):
+        return self.serialized
--- prodigy_site/classes/views.py
+++ prodigy_site/classes/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from .recommendations import *
 from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets
-from .serializers import ClassSerializer
+from .serializers import ClassSerializer, ClassModelSerializer
 from .models import Class, Review
 
 
 # @login_required(login_url='/accounts/login/')
 class ClassView(viewsets.ModelViewSet):
-    serializer_class = ClassSerializer
+    serializer_class = ClassModelSerializer
     queryset = Class.objects.all()
 
 
@@ -17,23 +17,7 @@ def ClassViewSet(request):
     queryset = Class.objects.all()
     classes = []
     for query in queryset:
-        serialized = ClassSerializer(query).data
-        reviews = Review.objects.filter(class_for=query)
-        teacher = query.teacher
-        subject = query.subject
-
-        serialized["subject"] = subject.name
-        serialized["teacher"] = teacher.user.username
-
-        if len(reviews) == 0:
-            serialized['avr_reviews'] = 0
-        else:
-            sum = 0
-            for review in reviews:
-                sum += review.rating
-
-            serialized['avr_reviews'] = sum/len(reviews)
-
-        classes.append(serialized)
+        serializer = ClassSerializer(query)
+        classes.append(serializer.get_serialized())
 
     return JsonResponse({'courses': classes})
