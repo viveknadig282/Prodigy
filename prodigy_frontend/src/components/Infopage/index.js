cd1706c2e38eee6d3b390a8e325f33d4cd179959
Aditya Pawar <aditya.pawar@warriorlife.net>
2021-01-17 16:41:19 -0800

    fixed merge conflicts

--- prodigy_frontend/src/components/Infopage/index.js
+++ prodigy_frontend/src/components/Infopage/index.js
@@ -3,11 +3,6 @@ import course from './info.json'
 import './style.css'
 import { withRouter } from "react-router";
 import StarRatings from 'react-star-ratings';
-<<<<<<< HEAD
-
-const Infopage = () => {
-    
-=======
 import axios from 'axios';
 import { ThemeConsumer } from 'styled-components';
 
@@ -35,7 +30,6 @@ class Infopage extends React.Component {
     }
 
     render() {
->>>>>>> d166a0c234d3dc764bc1091ee8f9f0f5047313ad
     return (
         <>
             <tr key={this.state.id}>
